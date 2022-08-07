acc78261c2892876e93b8e14a4bb71a1d23fe2e9
UnityTheCoder <108679939+UnityTheCoder@users.noreply.github.com>
2022-08-07 14:33:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import lib.wrapper as gw
 import re, os, sys
 from tabulate import tabulate
 import requests
+from colorama import *
+import shutil
+
 
 home = os.path.expanduser('~')
 
@@ -19,40 +22,53 @@ def setup():
         print(e)
 
 
-def add(name: str, desc: str):
+def add(name: str, desc: str, choosen):
     #re.findall('\[(.*?)\]',s)
     query = f"[{name}] [{desc}] [Uncomplete]"
-    open(f"{home}/.config/clorg/default.list", "a").write(query + "\n")
+    open(f"{home}/.config/clorg/{choosen}.list", "a").write(query + "\n")
 
 def read():
-    activities = []
-    list = open(f"{home}/.config/clorg/default.list", "r").read().strip()
-    lines = list.split("\n")
-    i=0
-    for line in lines:
-        ir = re.findall('\[(.*?)\]',line)
-        ko = [i]
-        for it in ir:
-            ko.append(it)
-        activities.append(ko)
-        i+=1
+    lists = retrieveLists()
+    for listx in lists:
+        lilen = len(listx)
+        ascii = f"""
 
-    all_data = [["ID", "Name", "Description", "Status"]
+
+    ╒═══════════════════════{"══" * int(lilen/2)}╕
+    │           {listx}     {" " * int(lilen - 1)}│
+    ╘═══════════════════════{"══" * int(lilen/2)}╛"""
+        activities = []
+        list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip()
+        if list == "":
+            continue
+        lines = list.split("\n")
+        i=0
+        for line in lines:
+            ir = re.findall('\[(.*?)\]',line)
+            ko = [i]
+            for it in ir:
+                ko.append(it)
+            activities.append(ko)
+            i+=1
+
+        all_data = [["ID", "Name", "Description", "Status"]
             ]
-    for activity in activities:
-        try:
-            activity[3] = activity[3].replace("Uncomplete", "").replace("Completed", "")
-            all_data.append(activity)
-        except:
-            return False
+        for activity in activities:
+            try:
+                activity[3] = activity[3].replace("Uncomplete", "").replace("Completed", "")
+                all_data.append(activity)
+                table = tabulate(all_data,headers='firstrow',tablefmt='fancy_grid')
+                print(ascii)
+                print(table)
+
+            except:
+                continue
+            
 
 
-    table = tabulate(all_data,headers='firstrow',tablefmt='fancy_grid')
 
-    print(table)
-
-def remove(id: int):
-    list = open(f"{home}/.config/clorg/default.list", "r").read().strip()
+def remove(id: int, listx):
+    list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip()
     lines = list.split("\n")
     newlist = ""
     i=0
@@ -62,11 +78,11 @@ def remove(id: int):
         else:
             newlist+=line+"\n"
         i+=1
-    open(f"{home}/.config/clorg/default.list", "w").write(newlist)
+    open(f"{home}/.config/clorg/{listx}.list", "w").write(newlist)
 
-def set(id: int, query: str):
+def set(id: int, query: str, listx):
     if query == "completed":
-        list = open(f"{home}/.config/clorg/default.list", "r").read().strip()
+        list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip()
         lines = list.split("\n")
         newlist = ""
         i=0
@@ -79,9 +95,9 @@ def set(id: int, query: str):
             else:
                 newlist+=line+"\n"
             i+=1
-        open(f"{home}/.config/clorg/default.list", "w").write(newlist)
+        open(f"{home}/.config/clorg/{listx}.list", "w").write(newlist)
     if query == "uncompleted":
-        list = open(f"{home}/.config/clorg/default.list", "r").read().strip()
+        list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip()
         lines = list.split("\n")
         newlist = ""
         i=0
@@ -94,19 +110,21 @@ def set(id: int, query: str):
             else:
                 newlist+=line+"\n"
             i+=1
-        open(f"{home}/.config/clorg/default.list", "w").write(newlist)
+        open(f"{home}/.config/clorg/{listx}.list", "w").write(newlist)
 
 
 
 
 
 def cliset():
-    list = open(f"{home}/.config/clorg/default.list", "r").read().strip().replace("Uncomplete", "").replace("Completed", "")
+    lists = retrieveLists()
+    listx = gw.choose(lists)
+    list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip().replace("Uncomplete", "").replace("Completed", "")
     lines = list.split("\n")
     selected = gw.choose(lines).replace("", "Uncomplete").replace("", "Completed")
-    opt = ["completed", "uncompleted"]
-    selected2 = gw.choose(opt)
-    list = open(f"{home}/.config/clorg/default.list", "r").read().strip()
+    opt = ["", ""]
+    selected2 = gw.choose(opt).replace("", "completed").replace("", "uncompleted")
+    list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip()
     lines = list.split("\n")
     i=0
     target = None
@@ -116,31 +134,86 @@ def cliset():
             break
         i+=1
 
-    set(target, selected2)
+    set(target, selected2, listx)
 
 
 
 def cliadd():
+    lists = retrieveLists()
+    listx = gw.choose(lists)
     name = gw.input("Name")
     desc = gw.input("Description")
-    add(name, desc)
+    add(name, desc, listx)
 
 
 def cliremove():
-    list = open(f"{home}/.config/clorg/default.list", "r").read().strip().replace("Uncomplete", "").replace("Completed", "")
+    lists = retrieveLists()
+    listx = gw.choose(lists)
+    list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip().replace("Uncomplete", "").replace("Completed", "")
     lines = list.split("\n")
     selected = gw.choose(lines).replace("", "Uncomplete").replace("", "Completed")
-    list = open(f"{home}/.config/clorg/default.list", "r").read().strip()
-    lines = list.split("\n")
-    i=0
-    target = None
-    for line in lines:
-        if line == selected:
-            target = i
-            break
-        i+=1
-    remove(target)
+    confirm = gw.confirm("Confirm?")
+    if confirm:
+        list = open(f"{home}/.config/clorg/{listx}.list", "r").read().strip()
+        lines = list.split("\n")
+        i=0
+        target = None
+        for line in lines:
+            if line == selected:
+                target = i
+                break
+            i+=1
+        remove(target, listx)
 
+
+
+def climport():
+    path = gw.input("path").replace("~", home)
+    if os.path.exists(path):
+        pass
+    else:
+        print(Fore.RED + "[-] Invalid Path!" + Fore.RESET)
+        return False
+    if path.endswith(".list"):
+        pass
+    else:
+        print(Fore.RED + "[-] File must be a .list!" + Fore.RESET)
+        return False
+    shutil.copy(path, f"{home}/.config/clorg/")
+    print(Fore.GREEN + "Imported!" + Fore.RESET)
+
+def retrieveLists():
+    path = home + "/.config/clorg/"
+    lists = []
+    for files in os.listdir(path):
+        filep = os.path.join(path, files)
+        if os.path.isfile(filep) and filep.endswith(".list"):
+            lists.append(files.replace(".list", ""))
+    return lists
+
+
+def cliexport():
+    lists = retrieveLists()
+    sel = gw.choose(lists, False)
+    for s in sel:
+        pfile = f"{home}/.config/clorg/{s}.list"
+        shutil.copy(pfile, home)
+        print(Fore.GREEN + "[+] Copied List in ~/" + s + ".list")
+
+
+
+
+
+def newlistcli():
+    name = gw.input("Name")
+    open(f"{home}/.config/clorg/{name}.list", 'w').write("")
+
+
+def removelistcli():
+    lists = retrieveLists()
+    choosen = gw.choose(lists, False)
+    for name in choosen:
+        os.remove(f"{home}/.config/clorg/{name}.list")
 
 
 
@@ -154,6 +227,10 @@ help = """
 - list
 - remove  
 - setup
+- import
+- export
+- newlist
+- removelist
 """
 
 
@@ -174,3 +251,13 @@ if __name__ == "__main__":
             setup()
         elif sys.argv[1] == "remove":
             cliremove()
+        elif sys.argv[1] == "import":
+            climport()
+        elif sys.argv[1] == "export":
+            cliexport()
+        elif sys.argv[1] == "newlist":
+            newlistcli()
+        elif sys.argv[1] == "removelist":
+            removelistcli()
+
+
